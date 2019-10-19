56b8dc654e98146a7ca8787a1f910578d4d27c76
Patrick Fic <patrick@thinkimex.com>
2019-10-18 17:05:18 -0700

    Publishing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
+
+    //this.handleChange = this.handleChange.bind(this);
+    //not necessary if using arrow functions
   }
 
   componentDidMount() {
@@ -17,10 +22,25 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  //setting this as an arrow function sets the scope correctly to the place where it was defined.
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField)
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import './card.style.css'
+import "./card.style.css";
 
-
-export const Card = (props) => {
-    console.log('this is m ', props.monster)
+export const Card = props => {
   return (
-    <div className='card-container'>
-    <img alt='Monster' src={`https://robohash.org/`+ props.monster.id +`?set=set2`}></img>
+    <div className="card-container">
+      <img
+        alt="Monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      ></img>
       <h1 key={props.monster.id}>{props.monster.name}</h1>
+      <h2>
+        <p>{props.monster.email}</p>
+      </h2>
     </div>
   );
 };
