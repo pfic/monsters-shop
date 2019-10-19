43bd9294a0eae09c044f3c443a78533790a0e24e
Patrick Fic <patrick@thinkimex.com>
2019-10-18 17:04:40 -0700

    Updates

---
+++ precache-manifest.6ec1128a40d5c193fd4f374dd21c0507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0214573bd66cc7d9274bba589e6d783d",
+    "url": "/monsters-shop/index.html"
+  },
+  {
+    "revision": "55abb3baeb90770423c8",
+    "url": "/monsters-shop/static/css/main.daefb2d0.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-shop/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "55abb3baeb90770423c8",
+    "url": "/monsters-shop/static/js/main.1c027fd7.chunk.js"
+  },
+  {
+    "revision": "3bca6a2e95edf95d13e1",
+    "url": "/monsters-shop/static/js/runtime-main.f542bb3a.js"
+  }
+]);
